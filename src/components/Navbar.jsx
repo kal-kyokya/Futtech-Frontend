87a9eb90ddac635c16748e7631f5e01dd82c585c
kal-kyokya <kalkyokya4@gmail.com>
2025-03-09 12:03:41 +0000

    Complete creation of the Navbar element.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,33 @@
 import './navbar.scss';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const Navbar = () => {
     return (
 	<div className='navbar'>
 	    <div className='container'>
 		<div className='left'>
-		    <img src='../../public/logo.png' alt='Logo of the Futtech Company'/>
+		    <img src='../../public/logo.png'
+			 alt='Logo of the Futtech Company'/>
+		    <span>Home</span>
+		    <span>Drone Footages</span>
+		    <span>AI-driven Analysis</span>
+		    <span>New & Popular</span>
 		</div>
 
 		<div className='right'>
-
+		    <SearchIcon className='icon'/>
+		    <NotificationsNoneIcon className='icon'/>
+		    <img src='../../public/profile.JPG'
+			 alt='Icon categorizing user as player, coach or scout'/>
+		    <div className='profile'>
+			<ArrowDropDownIcon className='icon'/>
+			<div className='options'>
+			    <span>Settings</span>
+			    <span>Logout</span>
+			</div>
+		    </div>
 		</div>
 	    </div>
 	</div>
