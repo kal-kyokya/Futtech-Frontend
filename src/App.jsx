51cab950bc41182ae4004665d148506df3b4b119
kal-kyokya <kalkyokya4@gmail.com>
2025-03-09 08:58:37 +0000

    Establish a black background color for the entire App and create the Navbar component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import './App.scss';
+import './app.scss';
+import Home from './home/Home';
 
 const App = () => {
-  return <div>Hello world!</div>;
+    return <Home/>;
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import './navbar.scss';
+
+const Navbar = () => {
+    return (
+	<div>
+	    Home Navbar.
+	</div>
+    );
+}
+
+export default Navbar;
