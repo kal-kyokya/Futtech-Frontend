805253809e5b716ab8f801523db270a7a393127b
kal-kyokya <kalkyokya4@gmail.com>
2025-03-10 14:55:37 +0000

    Implement Hover feature on Video section.

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,9 +1,40 @@
 import './listItem.scss';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AddIcon from '@mui/icons-material/Add';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ThumbDownOutlinedIcon from '@mui/icons-material/ThumbDownOutlined';
+import { useState } from 'react';
 
-const ListItem = () => {
+const ListItem = ({ index }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-	<div className='listItem'>
-	    List Item.
+	<div className='listItem'
+	     style={ {left: isHovered && index * 225 - 50 + index *  2.5} }
+	     onMouseEnter={ () => setIsHovered(true) }
+	     onMouseLeave={ () => setIsHovered(false) }>
+	    <img src='../../../public/congo.JPEG'
+		 alt='Website Owner proudly holding the Congolese flag'
+	    />
+	    <div className='itemInfo'>
+		<div className='icons'>
+		    <PlayArrowIcon/>
+		    <AddIcon/>
+		    <ThumbUpOutlinedIcon/>
+		    <ThumbDownOutlinedIcon/>
+		    <div className='properties'>
+			<span>Duration</span>
+			<span>Location</span>
+			<span>Date</span>
+		    </div>
+		    <div className='description'>
+			Culture Week games in USIU-Africa on 14th February 2025, representing Congo DRC and have the chance to play in the finals.
+		    </div>
+		    <div className='type'>
+			Game
+		    </div>
+		</div>
+	    </div>
 	</div>
     );
 }
