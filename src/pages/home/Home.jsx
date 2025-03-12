7a59a38ad7d4d2eb4579c7c9bebf129d1385cdd1
kal-kyokya <kalkyokya4@gmail.com>
2025-03-12 11:43:26 +0000

    Create a 'Watch' page for uploaded videos.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,15 +2,12 @@ import './home.scss';
 import Navbar from '../../components/Navbar';
 import Featured from '../../components/featured/Featured';
 import List from '../../components/list/List';
+import Watch from '../../pages/watch/Watch';
 
 const Home = () => {
     return (
 	<div className='home'>
-	    <Navbar/>
-	    <Featured/>
-	    <List/>
-	    <List/>
-	    <List/>
+	    <Watch />
 	</div>
     );
 }
